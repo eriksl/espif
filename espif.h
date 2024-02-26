2caa30fd7f20ed828d670b87724063e6199ab053
Erik Slagter <erik@openpli.org>
2024-02-26 12:32:49 +0100

    espif.cpp: make constructor argument port optional as well.

--- espif.h
+++ espif.h
@@ -14,7 +14,7 @@ class Espif
 {
 	public:
 
-		Espif(std::string host, std::string command_port,
+		Espif(std::string host, std::string command_port = "24",
 					bool verbose = false, bool debug = false, bool broadcast = false, bool multicast = false, bool use_tcp = false, bool raw = false,
 					bool no_provide_checksum = false, bool no_request_checksum = false, bool dontwait = false,
 					unsigned int broadcast_group_mask = 0, unsigned int multicast_burst = 0, unsigned int flash_sector_size = _flash_sector_size);
